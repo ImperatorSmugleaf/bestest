81dc9a6e13093bb2cee861cb8168f7571110d077
kirakira0 <kiratoal0@gmail.com>
2022-11-29 19:42:29 -0800

    Updating icon

--- popout/popup.js
+++ popout/popup.js
@@ -4,8 +4,18 @@ let b = document.getElementById("b");
 b.addEventListener("click", f);
 
 function f() {
-  chrome.tabs.create({
-    url: "index.html",
-  });
-  // alert("HELLO");
+  alert("hi");
+  chrome.extension.getBackgroundPage().console.log("foo");
+  //   let queryOptions = { active: true, lastFocusedWindow: true };
+  //   // `tab` will either be a `tabs.Tab` instance or `undefined`.
+  //   let [tab] = await chrome.tabs.query(queryOptions);
+  //   console.log(tab);
+  //   return tab;
 }
+
+// function f() {
+//   chrome.tabs.create({
+//     url: "index.html",
+//   });
+//   // alert("HELLO");
+// }
